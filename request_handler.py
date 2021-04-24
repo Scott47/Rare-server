a09a99acfaf4c8f42cb27c7aec3eb08cf879d420
Scott47 <scottsilver47@gmail.com>
2021-04-24 18:59:35 -0500

    created user model, users/request.py, and do_GET in request_handler. get_all_users and get_single_user return 200 status with encoded object in postman

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,41 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from users import (get_all_users,
+                get_single_user)
 
 class HandleRequests(BaseHTTPRequestHandler):
+
+    def parse_url(self, path):
+        # Just like splitting a string in JavaScript. If the
+        # path is "/animals/1", the resulting list will
+        # have "" at index 0, "animals" at index 1, and "1"
+        # at index 2.
+        path_params = path.split("/")
+        resource = path_params[1]
+        # Check if there is a query string parameter
+        if "?" in resource:
+            # GIVEN: /customers?email=jenna@solis.com
+
+            param = resource.split("?")[1]  # email=jenna@solis.com
+            resource = resource.split("?")[0]  # 'customers'
+            pair = param.split("=")  # [ 'email', 'jenna@solis.com' ]
+            key = pair[0]  # 'email'
+            value = pair[1]  # 'jenna@solis.com'
+
+            return ( resource, key, value )
+
+        # No query string parameter
+        else:
+            id = None
+
+        # Try to get the item at index 2
+        try:
+            id = int(path_params[2])
+        except IndexError:
+            pass  # No route parameter exists: /animals
+        except ValueError:
+            pass  # Request had trailing slash: /animals/
+
+        return (resource, id)  # This is a tuple
 
     # Here's a class function
     def _set_headers(self, status):
@@ -16,6 +51,28 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
         self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
         self.end_headers()
+
+    def do_GET(self):
+        # Set the response code to 'Ok'
+        self._set_headers(200)
+        response = {} #Default response
+        # Parse the URL and capture the tuple that is returned
+        parsed = self.parse_url(self.path)
+
+        if len(parsed) == 2:
+            (resource, id) = parsed
+
+            if resource == "users":
+                if id is not None:
+                    response = f"{get_single_user(id)}"
+                else:
+                    response = f"{get_all_users()}"
+
+        # Your new console.log() that outputs to the terminal
+        print(self.path)
+
+        self.wfile.write(f"{response}".encode())
+
 
 # This function is not inside the class. It is the starting
 # point of this application.
